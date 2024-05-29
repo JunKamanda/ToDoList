908412c728cb92ddcad3415ae6063f77b8b94104
Junior_Kamanda <jkamanda062gmail.com>
2024-05-29 14:59:25 +0200

    TP2_ToDoList

---
+++ TP2_TODOLIST/app.js
@@ -0,0 +1,9 @@
+//créer un form pour taper les todos
+//Ajouter sur le dom ce qui doit être tapé dans l'input à la validation du form
+//Mon Input.value
+
+//Supprimer un todo lorsque l'on click dessus .remove
+
+//Stocker dans le local storage la liste
+
+//Consulter le local storage au lancement de l'application pour rajouter les todos
